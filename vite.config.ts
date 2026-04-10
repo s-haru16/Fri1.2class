9604bd0a70c6aa9466865d1ebafb638e76121f87
s-haru16 <24im0056@i-u.ac.jp>
2026-04-10 02:49:47 +0000

    change

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react()],
+  // ⚠️ ここを追加：前後をスラッシュで囲むこと
   base: '/Fri1.2class/', 
 })
